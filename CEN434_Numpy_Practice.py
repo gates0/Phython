f7c3b6704dce1ff583da8a296279b494a80fa182
gates0 <142318144+gates0@users.noreply.github.com>
2024-01-11 13:47:53 +0100

    Update CEN434_Numpy_Practice.py

--- CEN434_Numpy_Practice.py
+++ CEN434_Numpy_Practice.py
@@ -5,11 +5,6 @@ b = np.array([1, 2, 3, 4, 5])
 c = np.array([[1, 2, 3], [4, 5, 6]])
 d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
-print(a.ndim)
-print(b.ndim)
-print(c.ndim)
-print(d.ndim)
-
 a = np.array([[1, 2],[4,5]])
 
 print(a)
